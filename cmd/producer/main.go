59d76352a0e865f972e8721becf2fe61f41f3da6
Carlos Martins <cmartins.dev@gmail.com>
2026-07-28 23:47:14 -0300

    feat: implement Kafka message publishing in Go producer

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -8,7 +8,12 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello Go!")
+	topic := "fullcycle-trilha-kafka"
+
+	producer := newKafkaProducer()
+	Publish("Hello, Kafka!", topic, producer, nil)
+	producer.Flush(15 * 1000)
+
 }
 
 func newKafkaProducer() *kafka.Producer {
@@ -22,4 +27,18 @@ func newKafkaProducer() *kafka.Producer {
 	}
 
 	return producer
+}
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte) error {
+	message := &kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          []byte(msg),
+		Key:            key,
+	}
+
+	err := producer.Produce(message, nil)
+	if err != nil {
+		return fmt.Errorf("failed to produce message: %v", err)
+	}
+
+	return nil
 }
